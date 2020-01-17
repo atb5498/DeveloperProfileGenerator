e52077857e45d63a225d3ef61dafb422cd29de84
abickford <atb5498@gmail.com>
2020-01-16 19:41:32 -0600

    created html structure

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 const axios = require("axios");
-// const fs = require("fs");
 
-inquirer.prompt([
+const writeFileAsync = util.promisify(fs.writeFile);
+
+return inquirer.prompt([
     {
         type: "input",
-        message: "What is your GitHub username?",
-        name: "username"
+        name: "username",
+        message: "What is your GitHub username?"
     },
     {
-        type: "input",
+        type: "list",
+        name: "color",
         message: "What is your favorite color?",
-        name: "color"
+        choices: ["blue", "gray", "green", "red", "yellow", "teal"]
     }
 ]).then(function ({ username }) {
-    const gitUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+    const gitUrl = `https://api.github.com/users/${username}`;
 
     axios.get(gitUrl).then(function (res) {
-        console.log(res.data);
-        // const profileDetails = res.data.map(function (profile) {
-        //     return profile.name;
-        // });
+        console.log(res.data.avatar_url);
+        console.log(res.data.name);
+        console.log(res.data.location);
+        console.log(res.data.url);
+        console.log(res.data.blog);
+        console.log(res.data.bio);
+        console.log(res.data.public_repos);
+        console.log(res.data.followers);
+        console.log(res.data.following);
+    });
 
-        //         const repoNamesStr = repoNames.join("\n");
+    const gitStarredUrl = `https://api.github.com/users/${username}/starred`;
 
-        //         fs.writeFile("repos.txt", repoNamesStr, function (err) {
-        //             if (err) {
-        //                 throw err;
-        //             }
-
-        //             console.log(`Saved ${repoNames.length} repos`);
-        //         });
+    axios.get(gitStarredUrl).then(function (res) {
+        console.log(res.data.length);
     });
 });
+
+// function generateHTML(answers) {
+// }
+
+// async function init() {
+//     try {
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// init();
